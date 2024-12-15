3185c46f2de9af8d9cdb4ee581a65e6dca45e535
Kirill Spesivtsev <ksby8819@gmail.com>
2024-12-15 13:50:01 +0100

    Containerized backend

---
+++ GisProxy/Helpers/MigrationHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GisProxy.Helpers;
+
+
+public static class MigrationHelper
+{
+	public static async Task ApplyMigrationsAsync<T>(this WebApplication app) where T : DbContext
+	{
+		using var scope = app.Services.CreateScope();
+		var context = scope.ServiceProvider.GetRequiredService<T>();
+		await context.Database.EnsureCreatedAsync();
+	}
+}
